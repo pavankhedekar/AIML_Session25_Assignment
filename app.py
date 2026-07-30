d46c631fff1e0e52a0dc76bae729e05fe558ed8a
pavankhedekars7875@gmail.com <pavankhedekars7875@gmail.com>
2026-07-30 13:32:32 +0530

    Iris Flower Prediction

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model=joblib.load("best_model.pkl")
+
+st.title("Iris Flower Prediction")
+
+sl=st.number_input("Sepal Length")
+sw=st.number_input("Sepal Width")
+pl=st.number_input("Petal Length")
+pw=st.number_input("Petal Width")
+
+if st.button("Predict"):
+
+    data=np.array([[sl,sw,pl,pw]])
+
+    pred=model.predict(data)
+
+    st.success(f"Prediction : {pred[0]}")
